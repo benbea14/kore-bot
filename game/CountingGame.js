474301e3c7b64ba7101d8aed7a5ebf7b251f6efc
benbea14 <betti-traum@web.de>
2026-02-22 04:19:52 +0100

    Fixing CountingGame.js

--- game/CountingGame.js
+++ game/CountingGame.js
@@ -1,4 +1,4 @@
-const { makeError, EmbedBuilder } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
 
 const COUNTING_CHANNEL_ID = process.env.COUNTING_CHANNEL_ID;
 
@@ -6,23 +6,23 @@ let currentNumber = 0;
 let lastUserId = null;
 
 async function countingGame(message) {
-    if (message.auther.bot) return;
+    if (message.author.bot) return;
     if (message.channel.id !== COUNTING_CHANNEL_ID) return;
 
     const number = parseInt(message.content);
     if (isNaN(number)) return;
 
-    if (message.auther.id === lastUserId) {
-        await message.react("🚫")
-        await message.reply("🚫 You can`t count twice in a row! Reset to 1.");
+    if (message.author.id === lastUserId) {
+        await message.react("🚫");
+        await message.reply("🚫 You can’t count twice in a row! Reset to 1.");
         currentNumber = 0;
         lastUserId = null;
         return;
     }
 
     if (number !== currentNumber + 1) {
-        await message.react("❌")
-        await message.reply(`❌ Wrong number! It was ${currentNumber + 1}. Reset to 1.'`);
+        await message.react("❌");
+        await message.reply(`❌ Wrong number! It was ${currentNumber + 1}. Reset to 1.`);
         currentNumber = 0;
         lastUserId = null;
         return;
@@ -34,16 +34,21 @@ async function countingGame(message) {
     await message.react("💜");
 
     // Milestones
-    if (currentNumber === 25 || currentNumber === 50 || currentNumber === 75 || currentNumber === 100);
-
+    if (
+        currentNumber === 25 ||
+        currentNumber === 50 ||
+        currentNumber === 75 ||
+        currentNumber === 100
+    ) {
         const embed = new EmbedBuilder()
-            .setColor("0x9B59B6")
-            .setTitle("A Milestone!")
-            .setDescription(`We just reached **${currentNumber}**! 💜'`)
-            .setFooter({ text: "ARMY!! Keep counting!!"})
+            .setColor(0x9B59B6)
+            .setTitle("🎉 A Milestone!")
+            .setDescription(`We just reached **${currentNumber}**! 💜`)
+            .setFooter({ text: "ARMY!! Keep counting!!" })
             .setTimestamp();
 
         await message.channel.send({ embeds: [embed] });
+    }
 }
 
 module.exports = countingGame;
