bbee44dc45712f7ce19c24792e58c3b2423e9703
benbea14 <betti-traum@web.de>
2026-03-22 01:04:13 +0100

    Update exportxp.js

--- commands/admin/exportxp.js
+++ commands/admin/exportxp.js
@@ -17,7 +17,7 @@ module.exports = {
     }
 
     try {
-      const dataPath = path.join(__dirname, '..', '..', 'XP', 'data.json');
+      const dataPath = '/XP/data.json';
 
       if (!fs.existsSync(dataPath)) {
         return interaction.reply({ 
